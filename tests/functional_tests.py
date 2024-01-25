1ecf2296c6492879e521f522d75fa860d5892b25
Vincent Lo <lovincent536@gmail.com>
2024-01-25 23:10:30 +0800

    Functional test now tests adding an item to the list

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,26 +1,41 @@
 import unittest
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVistorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        
+
     def tearDown(self):
         self.browser.quit()
-    
+
     def test_start_a_list_and_retrieve_it_later(self):
         # Edith has a few todo items, so she enter the url to the browser to launch the app
         self.browser.get('http://localhost:8000')
 
-        # She notices To-Do in the title of the page
+        # She notices To-Do in the title and the header of the page
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1')
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a todo item right away
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # 1. buy peacock feathers
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # There is another text box inviting her to enter another item.
         # She enters "Use peacock feathers to make a fly"
@@ -35,6 +50,7 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main()
