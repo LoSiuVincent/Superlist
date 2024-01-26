c98c7715264043eb2a659ba84bffb5fb26160e40
Vincent Lo <lovincent536@gmail.com>
2024-01-27 02:26:30 +0800

    The app now support multiple lists

--- lists/tests.py
+++ lists/tests.py
@@ -15,6 +15,7 @@ class HomePageTest(TestCase):
 
         self.assertEqual(Item.objects.count(), 0)
 
+
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
@@ -24,10 +25,35 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        
+
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        correct_list = List.objects.create()
+        other_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
 class ListViewTest(TestCase):
     def test_used_list_template(self):
@@ -50,12 +76,19 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other item 1')
         self.assertNotContains(response, 'other item 2')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
 
 class ListAndItemModelTest(TestCase):
     def test_save_and_retrieve_item(self):
         list_ = List()
         list_.save()
-        
+
         first_item = Item()
         first_item.text = 'The first item'
         first_item.list = list_
