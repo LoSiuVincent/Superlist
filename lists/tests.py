7742c5a9d732c8955e91884125902ebe36a9763f
Vincent Lo <lovincent536@gmail.com>
2024-01-26 19:02:19 +0800

    Added test for saving only when necessary

--- lists/tests.py
+++ lists/tests.py
@@ -20,6 +20,10 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_save_item_when_necessary(self):
+        self.client.post('/')
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
     def test_save_and_retrieve_item(self):
