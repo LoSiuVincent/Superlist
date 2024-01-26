a8979b02f9a7d3ed74ca247e16f5d7d23c40ddab
Vincent Lo <lovincent536@gmail.com>
2024-01-26 23:16:19 +0800

    Extended ItemModelTest to ListAndItemModelTest

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.test import TestCase
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your tests here.
@@ -43,21 +43,31 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'item 2')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_save_and_retrieve_item(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'The second item'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         self.assertEqual(first_saved_item.text, 'The first item')
+        self.assertEqual(first_saved_item.list, list_)
 
         second_saved_item = saved_items[1]
         self.assertEqual(second_saved_item.text, 'The second item')
+        self.assertEqual(second_saved_item.list, list_)
