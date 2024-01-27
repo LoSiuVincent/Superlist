f819a5ed32e386b31d78b3e2de2bad3080a46476
Vincent Lo <lovincent536@gmail.com>
2024-01-27 21:08:22 +0800

    Added CSRF trusted origins

--- superlists/settings.py
+++ superlists/settings.py
@@ -123,3 +123,5 @@ STATIC_ROOT = BASE_DIR / '.static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://ec2-3-27-169-52.ap-southeast-2.compute.amazonaws.com']
